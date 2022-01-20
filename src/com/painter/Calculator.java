3bd1bfa278899312defb539ec90f1f5c2beaddd1
BugMan420 <ismaelsim@hotmail.co.uk>
2022-01-20 16:08:21 +0000

    Price now uses the BigDecimal function

--- src/com/painter/Calculator.java
+++ src/com/painter/Calculator.java
@@ -1,4 +1,5 @@
 package com.painter;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Calculator {
@@ -7,22 +8,23 @@ public class Calculator {
         Scanner InputLine = new Scanner (System.in);
         String PaintName = "Bug Paint";
         int PaintCoveragePerCanInSqm = 8;
-
         System.out.println("Enter your first name please:");
         String UserName = InputLine.nextLine();
         System.out.println("How much does your paint cost?:");
-        double CostPerPaintCaninPence = Double.parseDouble(InputLine.nextLine());
+        BigDecimal CostPerPaintCaninPence = new BigDecimal(InputLine.nextLine());
         System.out.println("What is the total height of your walls in mm?");
         double WallHeightInmm = Double.parseDouble(InputLine.nextLine());
         System.out.println("What is the total width of your walls in mm?");
         double WallWidthInmm = Double.parseDouble(InputLine.nextLine());
         double WallSurfaceAreaInSqm = (WallHeightInmm/1000)*(WallWidthInmm/1000);
         int NumberOfPaintCans = (int) Math.ceil (WallSurfaceAreaInSqm/PaintCoveragePerCanInSqm);
-        
+        BigDecimal PaintCanNum = new BigDecimal(NumberOfPaintCans);
+        BigDecimal TotalPrice = CostPerPaintCaninPence.multiply(PaintCanNum);
+
         System.out.println(WallSurfaceAreaInSqm);
         System.out.println(NumberOfPaintCans);
 
         System.out.println("Welcome "+ UserName + ". To paint the house with " + PaintName + " you will need " + NumberOfPaintCans +
-                " cans of paint, which will cost you £" + (NumberOfPaintCans*(CostPerPaintCaninPence))+" Thank you!");
+                " cans of paint, which will cost you £" + TotalPrice  +" Thank you!");
     }
 }
